d66e74e0ffee7a83e7ba35da8b1e6a36a8dea2d7
Lou Ferrand <ferrandlou@gmail.com>
2011-06-25 00:08:21 +0200

    Add techno to database initialisation

--- tools/dbinit.js
+++ tools/dbinit.js
@@ -9,7 +9,9 @@ var log = new Log(config.logLevel);
 var Client = require('mysql').Client;
 var client = new Client(config.mysql);
 
-var dataTechPath = 'data/technologies_tags.csv';
+var dataTechPath  = 'data/technologies_tags.csv';
+var languagesPath = 'data/langues.csv';
+var metiersPath   = 'data/metiers.csv';
 
 
 log.info('Starting DbInit...');
@@ -19,12 +21,35 @@ client.connect();
 csv()
 .fromPath(dataTechPath)
 .on('data', function(data, index){
-	//La première ligne contient les colonne
-	if (index == 0) return;
+ //La première ligne contient les colonne
+ if (index == 0) return;
+ if (data[2] < 15) return;
+    log.info('Inserting #'+index+' '+JSON.stringify(data));
+ client.query(
+   'INSERT INTO technologie SET id_technologie = ?, tagname = ?, count = ?',
+   [index, data[1], data[2]],
+   function(err) {
+     if (err) log.warning(err);
+     log.info('Inserted #'+index+' '+JSON.stringify(data));
+   }
+ );
+})
+.on('end',function(count){
+    log.info('Inserted ' + count + ' lines');
+})
+.on('error',function(error) {
+    log.warning(error.message);
+ throw error;
+});
+
+
+csv()
+.fromPath(languagesPath)
+.on('data', function(data, index){
     log.info('Inserting #'+index+' '+JSON.stringify(data));
 	client.query(
-		'INSERT INTO technologie SET id_technologie = ?, libelle = ?',
-		[index, data[1]],
+		'INSERT INTO langue SET id_langue = ?, libelle = ?',
+		[index, data[0]],
 		function(err) {
 			if (err) log.warning(err);
 			log.info('Inserted #'+index+' '+JSON.stringify(data));

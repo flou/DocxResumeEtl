fb2eeb378065c76aec39e198397ba4e3571dc0c4
Lou Ferrand <ferrandlou@gmail.com>
2011-07-02 18:32:36 +0200

    Update dbinit, add truncate and new data files

--- tools/dbinit.js
+++ tools/dbinit.js
@@ -9,14 +9,35 @@ var log = new Log(config.logLevel);
 var Client = require('mysql').Client;
 var client = new Client(config.mysql);
 
-var dataTechPath  = 'data/technologies_tags.csv';
-var languagesPath = 'data/langues.csv';
-var metiersPath   = 'data/metiers.csv';
+var dataTechPath   = 'data/technologies_tags.csv';
+var languagesPath  = 'data/langues.csv';
+var metiersPath    = 'data/metiers.csv';
+var secteursPath   = 'data/secteurs.csv';
 
 
 log.info('Starting DbInit...');
 
 client.connect();
+
+log.info("Deleting all data...");
+client.query("TRUNCATE collaborateur_langue;");
+client.query("TRUNCATE collaborateur_secteur;");
+client.query("TRUNCATE competences_fonctionnelles;");
+client.query("TRUNCATE competences_techniques;");
+client.query("TRUNCATE diplome_certification;");
+client.query("TRUNCATE domaine_competences;");
+client.query("TRUNCATE mission_technologie;");
+client.query("TRUNCATE famille_metier;");
+client.query("TRUNCATE langue;");
+client.query("TRUNCATE mission;");
+client.query("TRUNCATE secteur;");
+client.query("TRUNCATE technologie;");
+client.query("TRUNCATE langue;");
+client.query("TRUNCATE collaborateur;");
+client.query("TRUNCATE client");
+log.info("Done deleting");
+
+log.info('Populating table "technologie"');
 
 csv()
 .fromPath(dataTechPath)
@@ -42,6 +63,7 @@ csv()
  throw error;
 });
 
+log.info('Populating table "langue"');
 
 csv()
 .fromPath(languagesPath)
@@ -64,5 +86,50 @@ csv()
 	throw error;
 });
 
+log.info('Populating table "famille_metier"');
+
+csv()
+.fromPath(metiersPath)
+.on('data', function(data, index){
+    log.info('Inserting #'+index+' '+JSON.stringify(data));
+  client.query(
+    'INSERT INTO famille_metier SET id_famille_metier = NULL, libelle = ?',
+    [data[0]],
+    function(err) {
+      if (err) log.warning(err);
+      log.info('Inserted #'+index+' '+JSON.stringify(data));
+    }
+  );
+})
+.on('end',function(count){
+    log.info('Inserted ' + count + ' lines');
+})
+.on('error',function(error) {
+    log.warning(error.message);
+  throw error;
+});
+
+log.info('Populating table "secteurs"');
+
+csv()
+.fromPath(secteursPath)
+.on('data', function(data, index){
+    log.info('Inserting #'+index+' '+JSON.stringify(data));
+  client.query(
+    'INSERT INTO secteur SET id_secteur = NULL, libelle = ?',
+    [data[0]],
+    function(err) {
+      if (err) log.warning(err);
+      log.info('Inserted #'+index+' '+JSON.stringify(data));
+    }
+  );
+})
+.on('end',function(count){
+    log.info('Inserted ' + count + ' lines');
+})
+.on('error',function(error) {
+    log.warning(error.message);
+  throw error;
+});
 
 log.info('DbInit Finished');
